32168dbc27fbae87a49919398d9e8a476a78f5bd
ChenYuHsin <s63232@gmail.com>
2018-04-04 12:33:03 +0800

    first commit

---
+++ src/components/test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="test">
+      test~~
+      {{msg}}
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "test",
+      data(){
+          return {
+            msg:"I'm data."
+          }
+      }
+    }
+</script>
+
+<style scoped>
+.test{
+  color: darkred;
+}
+</style>
