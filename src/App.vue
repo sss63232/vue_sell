4956d0a40796b58e940bbcd5c2fcc84c3d9561f3
YU-HSIN, CHEN <s63232@gmail.com>
2018-04-10 16:44:07 +0800

    add sentry

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,18 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-import test from './components/test';
+import HelloWorld from "./components/HelloWorld";
+import test from "./components/test";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
     test
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,38 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Raven from 'raven-js';
+import RavenVue from '../node_modules/raven-js/plugins/vue';
 
-Vue.config.productionTip = false
+Raven
+  .config('https://4f380245a3d344fe8debd0833e4af105@sentry.io/1186176')
+  .addPlugin(RavenVue, Vue)
+  .install();
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+Raven.context(function () {
+  Raven.setUserContext({
+    email: 'example@example.com',
+    id: 'example_101'
+  });
+
+  Raven.setTagsContext({
+    name: "newt"
+  });
+
+  Vue.config.productionTip = false
+
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    components: {
+      App
+    },
+    template: '<App/>'
+  })
+
+
+  // setTimeout(() => {
+  //   throw new Error('wrong');
+  // }, 5000);
+
+});
