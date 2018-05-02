693c7bb8cf8b6417564afb70170fca7cdb33d42b
YU-HSIN, CHEN <s63232@gmail.com>
2018-05-02 19:33:48 +0800

    update

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,21 +1,36 @@
 <template>
   <div class="header">
-    {{poiInfo.name}}
+    <div class="top-wrapper">
+      <div class="back-wrapper">
+        <div class="icon-arrow_lift"></div>
+      </div>
+      <form class="search-wrapper">
+        <span class="search-icon"></span>
+        <input class="search-bar" type="text" placeholder="搜索">
+      </form>
+      <div class="more-wrapper">
+        <a href="#" class="spelling-bt">拼單</a>
+        <div class="more-bt"></div>
+      </div>
+    </div>
+
+    <div class="content-wrapper"></div>
+
+    <div class="bulletin-wrapper"></div>
   </div>
 </template>
 
 <script>
-  export default {
-    props: {
-      poiInfo: {
-        type: Object,
-        default: {},
-      },
-    },
-  };
+export default {
+  props: {
+    poiInfo: {
+      type: Object,
+      default: {}
+    }
+  }
+};
 </script>
 
 <style>
-
+@import url("Header.css");
 </style>
-
